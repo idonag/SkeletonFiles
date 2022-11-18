bde28e00db95cbc2684491d46e5152e32dd6a2aa
yagil <yagilbr@post.bgu.ac.il>
2022-11-18 18:58:54 +0200

    1

--- src/Party.cpp
+++ src/Party.cpp
@@ -7,24 +7,16 @@ Party::Party(int id, string name, int mandates, JoinPolicy *jp) : mId(id), mName
     agentOffers = new vector<Agent>();
 
 }
-Party::Party(Party &other){ // copy constructor
-    mId=other.mId;
-    mName=other.mName;
-    mMandates=other.mMandates;
-    mState=other.mState;
+Party::Party(Party &other): mId(other.mId),mName(other.mName),mMandates(other.mMandates),mState(other.mState),coalition(other.coalition),iterNum(other.iterNum){ // copy constructor
     agentOffers = new vector<Agent>();
-    for(Agent a: *(other.agentOffers)){//copy the pointers to the agents to the new vector
+    for(Agent& a: other.agentOffers){//copy the pointers to the agents to the new vector
             agentOffers->push_back(a);
     }
 
 
 }
-Party::Party(Party &&other){//move constructor - shallow copy
-    mId=other.mId;
-    mName=other.mName;
-    mMandates=other.mMandates;
-    mState=other.mState;
-    agentOffers=other.agentOffers;
+Party::Party(Party &&other): mId(other.mId),mName(other.mName),mMandates(other.mMandates),mState(other.mState),coalition(other.coalition),iterNum(other.iterNum){//move constructor - shallow copy
+   agentOffers = other.agentOffers;
 }
 
 Party::~Party(){//destructor
@@ -33,11 +25,36 @@ Party::~Party(){//destructor
 }
 Party& Party::operator=(const Party &other){ // copy assignment opertor
     if(this != &other){
-//delete this resources
+        if(agentOffers)
+            delete agentOffers;
+        mId = other.mId;
+        mName=other.mName;
+        mMandates = other.mMandates;
+        mState = other.mState;
+        coalition = other.coalition;
+        iterNum = other.iterNum;
+        agentOffers = new vector<Agent> ();
+        for(Agent a: other.agentOffers){
+            
+        }
     }
+    return *this;
 }
 Party& Party::operator=(const Party &&other){ // move assignment opertor
+if(this!=&other){
+    if(agentOffers)
+        delete agentOffers;
+    agentOffers=other.agentOffers;
+    mId = other.mId;
+    mName=other.mName;
+    mMandates = other.mMandates;
+    mState = other.mState;
+    coalition = other.coalition;
+    iterNum = other.iterNum;
 
+}
+
+return *this;
 }
 
 State Party::getState() const
@@ -56,6 +73,8 @@ void Party::setState(State state)
 
 void Party::offer(const Agent &offer){
     agentOffers->push_back(offer);
+    if(mState!=CollectingOffers)
+        mState=CollectingOffers;
 }
 
 int Party::getMandates() const
@@ -68,10 +87,14 @@ const string & Party::getName() const
     return mName;
 }
 
-void Party::joinCoalition(const Agent & chosenAgent, const Simulation &a){
+void Party::joinCoalition(int chosenAgentIndex, Simulation &a){
     
-    a.g
+    Agent temp =a.getAgents().at(chosenAgentIndex);
+    int indexGenerated = a.getAgents().size();
 
+    a.getAgents().push_back(new Agent(indexGenerated,mId,new SelectionPolicy(temp.getSelectionPolicy())));
+    
+    coalition = a.getAgents().at(chosenAgentIndex).getCoalition();                                                                                                                                                                                                                                       
     mState=Joined;
 }
 
@@ -79,9 +102,8 @@ void Party::step(Simulation &s) //when joining coalition, clone the offering age
 {
     if(mState==CollectingOffers){
         if(iterNum==3){
-            const Agent &a= mJoinPolicy->chooseAgent(*agentOffers); // join some coalition by the joinPolicy
-            joinCoalition(a,s);
-            mState=Joined;
+            int chosenAgentIndex= mJoinPolicy->chooseAgent(s,*agentOffers); // join some coalition by the joinPolicy
+            joinCoalition(chosenAgentIndex,s);
         }
         else
             iterNum++;
